e16a212a8731b8f6cf76eeb087cf5b827baa56c0
Einar Persson <me@einarpersson.com>
2022-03-21 13:58:50 +0100

    Init

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+	// <style>`${foo}`</style>
+</script>
+
+<h1>Hello. Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+</style>
